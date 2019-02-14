e3873626076507afb5c95079a82cdec0a75feb77
siddharth2000 <40006080+siddharth2000@users.noreply.github.com>
2019-02-14 15:07:50 +0530

    Update code.py

--- codes/code.py
+++ codes/code.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 len = 1000
 r = np.sqrt(37)              #radius of the given circle
 A = np.array([0,-6])         #centre of the circle
-R= np.array([2,-4])          #nearest point to the centre of given circle
 Z = np.zeros((2,len))
 Y = np.zeros((2,len))
 X = np.zeros((2,len))
@@ -12,13 +11,17 @@ p = np.linspace(-20,20,len)
 t = np.linspace(0,2*np.pi,len)
 coeff = [4,0,2,6]          #normal equation is y+tx=4t+2t^3 and passes through centre of circle
 H = np.roots(coeff)
-S = np.real(H[np.isreal(H)])        #slope of the normal
+S = np.real(H[np.isreal(H)])[0]       #slope of the normal
+R= np.array([2*(S**2),4*S])          #nearest point to the centre of given circle
 r1= np.sqrt(((A[0]-R[0])**2)+((A[1]-R[1])**2))                #radius of required circle
 for i in range(len):
 	Z[:,i] = [(p[i]**2)/8,p[i]]                               #parabola in matrix form
 	Y[:,i] = [A[0]+r*np.cos(t[i]),A[1]+(r*np.sin(t[i]))]      #given circle
 	X[:,i] = [R[0]+r1*np.cos(t[i]),R[1]+(r1*np.sin(t[i]))]    #required circle
-	N[:,i] = A + p[i]*(S)               #normal line at point R
+	N[:,i] = R + p[i]*np.array([-S,1])               #normal line at point R
+plt.axis('scaled')
+plt.xlim(-10, 20)
+plt.ylim(-20, 20)
 plt.title('circle with centre on parabola passing through centre')
 plt.plot(Z[0,:],Z[1,:],label = '$y^2=8x$')
 plt.plot(Y[0,:],Y[1,:],label = '$x^2+y^2+12Y=1$')
